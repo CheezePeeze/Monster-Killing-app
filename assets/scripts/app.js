ee39a6e4edbeaf38142ede2c239f7a7886cbea99
VladysslavTkach <tkachacc.program@gmail.com>
2023-07-14 12:23:29 +0200

    new logic added to game functionality

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,1 +1,45 @@
- 
+const ATTACK_VALUE = 5;
+const MONSTER_ATTACK_VALUE = 9;
+const STRONG_ATTACK_VALUE = 14;
+const HEAL_VALUE = 20;
+
+let setMaxLife = 100;
+let currentMonsterHealth = setMaxLife;
+let currentPlayerHealth = setMaxLife;
+
+adjustHealthBars(setMaxLife);
+
+function endRound() {}
+
+function attackMonster(mode) {
+	let maxDamage;
+	if (mode === "ATTACK") {
+		maxDamage = ATTACK_VALUE;
+	} else if (mode === "STRONG_ATTACK") {
+		maxDamage = STRONG_ATTACK_VALUE;
+	}
+	const damage = dealMonsterDamage(maxDamage);
+	currentMonsterHealth -= damage;
+	const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
+	currentPlayerHealth -= playerDamage;
+	if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
+		alert("You won");
+	} else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
+		alert("Monster win");
+	} else if (currentPlayerHealth <= 0 && currentMonsterHealth <= 0) {
+		alert("You have a draw ");
+	}
+}
+function attackHandler() {
+	return attackMonster("ATTACK");
+}
+function strongAttackHander() {
+	return attackMonster("STRONG_ATTACK");
+}
+function healPlayerHandler() {
+	increasePlayerHealth(HEAL_VALUE);
+}
+
+attackBtn.addEventListener("click", attackHandler);
+strongAttackBtn.addEventListener("click", strongAttackHander);
+healBtn.addEventListener("click", healPlayerHandler);
